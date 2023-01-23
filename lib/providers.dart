3629473fce5d86bd96c8fd1f9cc01b91429007d1
Lucas.SilverPC <lucas.pereira@silvergames.com.br>
2023-01-23 14:34:17 -0300

    instanciando função para setup de serviços

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,18 +1,14 @@
 import 'package:flutter_api_demo/pages/users/users_controller.dart';
 import 'package:flutter_api_demo/repositories/users_repository.dart';
 import 'package:flutter_api_demo/services/auth_service.dart';
-import 'package:provider/provider.dart';
+import 'package:get_it/get_it.dart';
 
-final authService = AuthService();
+final getIt = GetIt.instance;
 
-final providers = [
-  Provider<UsersRepository>(
-    create: (context) => UsersRepository(),
-  ),
-  Provider<UsersController>(
-    create: (context) => UsersController(
-      usersRepository: context.read<UsersRepository>(),
-    ),
-  ),
-  ChangeNotifierProvider<AuthService>.value(value: authService),
-];
+setupProviders() {
+  getIt.registerLazySingleton<UsersRepository>(() => UsersRepository());
+  getIt.registerLazySingleton<UsersController>(() => UsersController(
+        usersRepository: getIt<UsersRepository>(),
+      ));
+  getIt.registerSingleton<AuthService>(AuthService());
+}
