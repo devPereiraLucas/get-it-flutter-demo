b827eb199a8dba40ce0f05bce725d7590ccd40e5
Lucas.SilverPC <lucas.pereira@silvergames.com.br>
2023-01-23 14:49:56 -0300

    inserindo a chamada do setupProvider na função main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_api_demo/app.dart';
 import 'package:flutter_api_demo/providers.dart';
-import 'package:provider/provider.dart';
 
 void main() {
+  setupProviders();
   runApp(
-    MultiProvider(
-      providers: providers,
-      child: const App(),
-    ),
+    const App(),
   );
 }
